aa3acf5699cacf6b11f54480597dee1b876a94fd
Thiago <thiagovt.dev@gmail.com>
2024-01-09 20:47:17 -0300

    refactoring

--- src/admin/index.ts
+++ src/admin/index.ts
@@ -4,10 +4,10 @@ import AdminJsSequelize from "@adminjs/sequelize";
 import { database } from "../database/index.js";
 import { adminJsResource } from "./resources/index.js";
 import { Components, componentLoader } from "./componentLoader.js";
-import { User } from "../models/Users.js";
-import bcrypt from "bcrypt";
 import { locale } from "./locale.js";
 import { dashboardHandler } from "./handlers/dashboard.js";
+import brandingOptions from "./features/branding.js";
+import authenticationOptions from "./features/authentication.js";
 
 
 AdminJs.registerAdapter(AdminJsSequelize);
@@ -17,27 +17,7 @@ export const adminJs = new AdminJs({
   rootPath: "/admin",
   resources: adminJsResource,
   componentLoader,
-  branding: {
-    companyName: "OneBitFlix",
-    logo: "/onebitflix.svg",
-    theme: {
-      colors: {
-        primary100: "#ff0043",
-        primary80: "#ff1a57",
-        primary60: "#ff3369",
-        primary40: "#ff4d7c",
-        primary20: "#ff668f",
-        grey100: "#151515",
-        grey80: "#333333",
-        grey60: "#4d4d4d",
-        grey40: "#666666",
-        grey20: "#dddddd",
-        filterBg: "#333333",
-        accent: "#151515",
-        hoverBg: "#151515",
-      },
-    },
-  },
+  branding: brandingOptions,
   locale, 
   dashboard :{
     component: Components.Dashboard,
@@ -47,18 +27,7 @@ export const adminJs = new AdminJs({
 
 export const adminJsRouter = AdminJsExpress.buildAuthenticatedRouter(
   adminJs,
-  {
-    authenticate: async (email, password) => {
-      const user = await User.findOne({ where: { email } });
-
-      if (user && user.role === 'admin') {
-        const matched = await bcrypt.compare(password, user.password);
-        if (matched) return user;
-      }
-      return false;
-    },
-    cookiePassword: "senha-de-cookie",
-  },
+  authenticationOptions,
   null,{
     resave: false,
     saveUninitialized: false,
