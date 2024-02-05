cb9c053492c5a09b9ae6f4c6409f55a2bc684330
Thiago <thiagovt.dev@gmail.com>
2024-02-05 08:53:45 -0300

    deleted .adminjs folder and add to gitignore

---
+++ .adminjs/.entry.js
@@ -0,0 +1,9 @@
+AdminJS.UserComponents = {}
+import Dashboard from '../dist/admin/components/Dashboard'
+AdminJS.UserComponents.Dashboard = Dashboard
+import UploadEditComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadEditComponent'
+AdminJS.UserComponents.UploadEditComponent = UploadEditComponent
+import UploadListComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadListComponent'
+AdminJS.UserComponents.UploadListComponent = UploadListComponent
+import UploadShowComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadShowComponent'
+AdminJS.UserComponents.UploadShowComponent = UploadShowComponent
